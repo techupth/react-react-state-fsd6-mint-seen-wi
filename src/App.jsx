f79d6dce1e86daf6b744beeb8269b84935e73f39
S33N <145544074+rizzboislw@users.noreply.github.com>
2024-06-05 11:46:14 +0700

    feat: added functional greeting buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
 import "./App.css";
+import {useState} from "react"
 
 function App() {
+
+  const [ greeting, setGreeting ] = useState("Greeting Message")
+
+  const setThaiGreeting = () => {
+    setGreeting('สวัสดี!')
+  }
+  const setEngGreeting = () => {
+    setGreeting('Hi!')
+  }
+  const setChineseGreeting = () => {
+    setGreeting('你好!')
+  }
+
+
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{greeting}</div>
       <div className="buttons">
-        <button>สวัสดี!</button>
-        <button>Hi!</button>
-        <button>你好!</button>
+        <button onClick={setThaiGreeting}>สวัสดี!</button>
+        <button onClick={setEngGreeting}>Hi!</button>
+        <button onClick={setChineseGreeting}>你好!</button>
       </div>
     </div>
   );
